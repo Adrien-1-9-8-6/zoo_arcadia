36aae4579df8b01dd97ebfa59504806babf720ca
Adrien-1-9-8-6 <adrien.cavet@hotmail.fr>
2024-08-11 23:20:19 +0200

    Page habitat en dynamique sur la table habitat, animal et race.

--- src/Controller/HabitatController.php
+++ src/Controller/HabitatController.php
@@ -10,22 +10,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
-
 class HabitatController extends AbstractController
 {
     #[Route('/habitat', name: 'app_habitat')]
     public function index(EntityManagerInterface $entityManager): Response
     {
-        $habitat1 = $entityManager->getRepository(Habitat::class)->find($id = 3);
-        $habitat2 = $entityManager->getRepository(Habitat::class)->find($id = 4);
-        $habitat3 = $entityManager->getRepository(Habitat::class)->find($id = 5);
+        $habitats = $entityManager->getRepository(Habitat::class)->findAll();
+        $animals = $entityManager->getRepository(Animal::class)->findAll();
 
         return $this->render('habitat/index.html.twig', [
-            'habitat1' => $habitat1,
-            'habitat2' => $habitat2,
-            'habitat3' => $habitat3,
-
+            'habitats' => $habitats,
+            'animals' => $animals,
         ]);
     }
-
 }
